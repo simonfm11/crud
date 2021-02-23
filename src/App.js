a8a8d6f662e450cd809b106075388aa0dfe10b8a
SIMON FLOREZ MONTES <simonflorez207934@correo.itm.edu.co>
2021-02-23 00:20:50 -0500

    Finish memory CRUD

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React , {useState} from 'react'
-import { isEmpty } from 'lodash'
-import shortid from 'shortid'
+import { isEmpty,size } from 'lodash'
+import shortid, { isValid } from 'shortid'
 
 function App() {
   const [task, setTask] = useState("")
   const [tasks, setTasks] = useState([])
+  const [editMode, setEditMode] = useState(false)
+  const [id, setId] = useState("")
+  const [error, setError] = useState(null)
+
+  const validForm = () => {
+     let isValid = true  
+     setError(null)
+
+    if(isEmpty(task)){
+      setError("Debes ingresar una tarea")
+      isValid(false)
+    }
+    return isValid
+  } 
 
   const addTask = (e) =>{
     e.preventDefault()
-    if(isEmpty(task)){
-      console.log("Task empty")
-      return
+
+   if(!validForm()) {
+       return
     }
 
     const newTask = {
@@ -22,6 +36,32 @@ function App() {
     setTasks([...tasks , newTask ])
     setTask("")
   }
+
+  const saveTask = (e) =>{
+    e.preventDefault()
+
+    if(!validForm()) {
+      return
+   }
+  
+  const editedTasks = tasks.map(item => item.id === id ? {id, name: task  } : item)
+    setTasks(editedTasks)
+    setEditMode(false)
+    setTask("")
+    setId("")
+  }
+
+  const deleteTask = (id) => {
+     const filteredTasks = tasks.filter(task => task.id !== id )
+     setTasks(filteredTasks)
+  }
+
+  const editTask = (theTask) => {
+   setTask(theTask.name)
+   setEditMode(true)
+   setId(theTask.id)
+ }
+
   return (
     <div className="container mt-5">
       <h1> Tareas</h1>
@@ -29,22 +69,42 @@ function App() {
       <div className="row">
         <div className="col-8">
          <h4 className="text-center"> Lista de Tareas </h4>
+         {
+           /*Este codigo es un if else para validar existencia de tareas */ 
+           size(tasks) == 0 ? (                
+             <li className="list-group-item">Aun no hay tareas programadas</li>
+           ) : (
          <ul className="list-group">
            {
-              tasks.map((task) => ( 
+              tasks.map((task) => (  
             <li className="list-group-item" key={task.id}>
             <span className="lead">{task.name} </span>
-            <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-            <button className="btn btn-warning btn-sm float-right">Editar</button>
+            <button 
+            className="btn btn-danger btn-sm float-right mx-2"
+            onClick={() => deleteTask(task.id)}
+            >
+              Eliminar
+              </button>
+            <button className="btn btn-warning btn-sm float-right"
+            onClick={() => editTask(task)}
+            >
+              Editar
+              </button>
            </li> 
               ))
            }
-
          </ul>
+              )
+          } 
        </div>
        <div className="col-4">
-         <h4 className="text-center"> Formulario </h4>
-         <form onSubmit={addTask}>
+         <h4 className="text-center">
+            { editMode ? "Modificar Tarea" : "Agregar tarea"} 
+         </h4>
+         <form onSubmit={editMode ? saveTask :  addTask}>
+           {
+             error && <span className="text-danger mb-2">{ error }</span>
+           }
            <input 
            type="text"
            className="form-control mb-2"
@@ -52,15 +112,17 @@ function App() {
            onChange={(text) => setTask(text.target.value)}
            value={task}
           />
-          <button className="btn btn-dark btn-block"
-                  type="submit">
-            Agregar la tarea
+          <button 
+          className={ editMode ? "btn btn-warning btn-block" : "btn btn-dark btn-block"}
+          type="submit"
+          >
+            { editMode ? "Guardar" : "Agregar" } 
           </button>
          </form>
        </div>
       </div>
     </div>
   )
-}
+ }
 
 export default App
